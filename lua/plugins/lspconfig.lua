3b77fd62cd5c160aa58445bb45637bf6e72d23e9
Ahmad Tanwir <contact@ata.web.id>
2024-01-13 22:38:46 +0700

    Add auto format and manual format by LSP

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -18,11 +18,22 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         config = function()
             local lspconfig = require("lspconfig")
+
             lspconfig.lua_ls.setup {}
             lspconfig.solargraph.setup {}
             lspconfig.pyright.setup {}
             lspconfig.gopls.setup {}
 
+            -- Auto Format only for some extensions
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = { "*.lua", "*.py", "*.go" },
+                buffer = buffer,
+                callback = function()
+                    vim.lsp.buf.format { async = false }
+                end
+            })
+            -- Manual Format on demand
+            vim.keymap.set("n", "<leader>F", vim.lsp.buf.format, { desc = "Format by LSP" })
         end,
     }
 }
