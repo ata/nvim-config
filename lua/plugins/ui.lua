9edf5f27ad79a3babaa26a1db219814eba7a91e1
Ahmad Tanwir <ahmad.tanwir@vidio.com>
2024-01-17 06:43:03 +0000

    Set full path for lualine filename

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,6 +2,18 @@ return {
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = {},
+        lazy = false,
+        config = function()
+            require('lualine').setup {
+                sections = {
+                    lualine_c = {
+                        {
+                            'filename',
+                            path = 1,
+                        }
+                    }
+                }
+            }
+        end
     }
 }
