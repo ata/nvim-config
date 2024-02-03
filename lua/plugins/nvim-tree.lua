ef3bb0764651b79318cad70828170f178529e6a1
Ahmad Tanwir <contact@ata.web.id>
2024-02-03 17:40:11 +0700

    Update nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,10 +5,11 @@ return {
         require("nvim-tree").setup {}
 
         -- Keymaps
-        local map  = vim.keymap.set
+        local map = vim.keymap.set
         local api = require("nvim-tree.api")
 
         map('n', '<leader>ee', api.tree.toggle, { desc = "Toggle Exporer" })
-        map('n', '<leader>ef', api.tree.find_file, { desc = "Find file in exporer" })
+        map('n', '<leader>ef', ":NvimTreeFindFile<cr>", { desc = "Find file in exporer" })
+        map('n', '<leader>eF', ":NvimTreeFindFile!<cr>", { desc = "Find file in exporer!" })
     end
 }
