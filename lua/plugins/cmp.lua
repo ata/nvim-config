731f6e4d28840b460c9ecb9d63a687c748a90bb0
Ahmad Tanwir <contact@ata.web.id>
2024-05-26 08:44:28 +0700

    Use arrow and enter for cmp navigation, so it will not conflict with tabnine plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -33,36 +33,36 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-                ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-                ["<Up>"] = cmp.mapping.scroll_docs(-4),
-                ["<Down>"] = cmp.mapping.scroll_docs(4),
+                ["<Up>"] = cmp.mapping.select_prev_item(),   -- previous suggestion
+                ["<Down>"] = cmp.mapping.select_next_item(), -- next suggestion
+                ["<C-Up>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-Down>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
                 ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-                        -- that way you will only jump inside the snippet region
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
+                -- ["<Tab>"] = cmp.mapping(function(fallback)
+                --     if cmp.visible() then
+                --         cmp.select_next_item()
+                --         -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                --         -- that way you will only jump inside the snippet region
+                --     elseif luasnip.expand_or_jumpable() then
+                --         luasnip.expand_or_jump()
+                --     elseif has_words_before() then
+                --         cmp.complete()
+                --     else
+                --         fallback()
+                --     end
+                -- end, { "i", "s" }),
 
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
+                -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+                --     if cmp.visible() then
+                --         cmp.select_prev_item()
+                --     elseif luasnip.jumpable(-1) then
+                --         luasnip.jump(-1)
+                --     else
+                --         fallback()
+                --     end
+                -- end, { "i", "s" }),
 
             }),
             -- sources for autocompletion
